0c01628390c0dc8df5ef4630a69519f6f2110bed
Ken Smith <kennethologist@gmail.com>
2022-05-09 20:11:40 -0400

    Added Data and setup up project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,26 @@
 import React from 'react'
 
+let myTodos = [
+  {
+    name: 'Organize Garage',
+    id: 1528817077286, // could look different, you could use a timestamp to generate it
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+]
+
+
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: myTodos
+    }
+  }
   render() {
     return (
       <div>
